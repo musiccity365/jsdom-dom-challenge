795cd4d64fd32d377b45bd39a6ef3e724002e6f9
musiccity365 <m_sackstein@yahoo.com>
2021-07-19 19:05:52 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,112 +1,61 @@
-// 1. As a user, I should see the timer increment every second once the page has
-// loaded.
+const counter = document.getElementById('counter')
+const plus = document.querySelector("#\\+")
+const minus = document.getElementById('-')
+const heart = document.getElementById('<3')
+const pause = document.getElementById('pause')
+const likes = document.querySelector('ul.likes')
+const comment_form = document.querySelector('#comment-form')
+const comments = document.querySelector('#list')
 
-const counter = document.getElementById('counter');
-const plusButton = document.getElementById('plus');
-const minusButton = document.getElementById('minus');
-const heartButton = document.getElementById('heart');
-const numberLikes = document.getElementById('.likes');
-const likedNumbers = {};
-const pauseButton = document.getElementById('pause');
-const restartButton = document.getElementById('restart');
-const submitButton = document.getElementById('submit')
-const commentForm = document.getElementsByTagName('input')[0]
-const numberComments = document.getElementById('.comments')
-let speedCounter = setInterval(plusCounter, 1000)
-let gameRunning = true
+let paused = false
+let numberTracker = {}
+let interval = setInterval(incrementCounter, 1000)
+plus.addEventListener("click", incrementCounter)
+minus.addEventListener("click", decrementCounter)
+pause.addEventListener("click", togglePaused)
+heart.addEventListener("click", addLike)
+comment_form.addEventListener("submit", handleSubmit)
 
-function plusCounter() {
-    if (gameRunning === true) {
-        counter.innerHTML++
-    }
-};
-
-function incrementTimer() {
-    let currentNumber = counter.innerText;
-    counter.innerText = parseInt(currentNumber) + 1;
+function incrementCounter(event) {
+    counter.innerText = parseInt(counter.innerText) + 1
 }
 
-function decrementTimer() {
-    let currentNumber = counter.innerText;
-    counter.innerText = parseInt(currentNumber) - 1;
+function decrementCounter() {
+    counter.innerText = parseInt(counter.innerText) - 1
 }
 
-function autoIncrement() {
-    setInterval(incrementTimer, 1000);
-}
-
-function likeNumber() {
-    //currentNumber has been liked x times
-    const currentNumber = counter.innerText;
-    if (likedNumbers[counter.innerHTML]) {
-        likedNumbers[counter.innerHTML] += 1;
-        document.getElementById(currentNumber).innerText = `${currentNumber} has been liked
-        ${likedNumbers[currentNumber]} times`;
+function togglePaused() {
+    paused = !paused
+    if (paused) {
+        clearInterval(interval)
+        pause.innerText = "resume"
     } else {
-        likedNumbers[counter.innerHTML] = 1;
-        numberLikes.innerHTML += `${counter.innerText} has been liked
-        ${likedNumbers[currentNumber]} times`;
+        interval = setInterval(incrementCounter, 1000)
+        pause.innerText = "pause"
     }
 }
 
-function addComment() {
-    e.preventDefault();
-    const p = document.createElement("p")
-    let commentInfo = document.createTextNode(`${commentForm.value}`);
-    newComment.appendChild(commentInfo);
-    comments.appendChild(newComment);
-    commentForm.value = '';
+function addLike() {
+    let second = counter.innerText
+    numberTracker[second] = numberTracker[second] || 0
+    numberTracker[second] += 1
+    renderLikes()
 }
 
-
-function pauseTimer() {
-
-    if (gameRunning === true) {
-        gameRunning = false
-        pause.innerHTML = "resume";
-        like.disabled = true;
-        minus.disabled = true;
-        plus.disabled = true;
-        submit.disabled = true;
-        commentForm.disabled = true;
-    } else {
-        gameRunning = true;
-        pause.innerHTML = "pause"
-        like.disabled = false;
-        minus.disabled = false;
-        plus.disabled = false;
-        submit.disabled = false;
-        commentForm.disabled = false;
-    };
-
+function renderLikes() {
+    likes.innerHTML = ""
+    for (let key in numberTracker) {
+        const li = document.createElement("li")
+        li.innerText = `${key} has been liked ${numberTracker[key]} times.`
+        likes.append(li)
+    }
 }
 
-
-// 2. As a user, I can manually increment and decrement the counter using the plus
-// and minus buttons.
-plusButton.addEventListener('click', incrementTimer);
-minusButton.addEventListener('click', decrementTimer);
-
-// 3. As a user, I can 'like'
-// an individual number of the counter.I should see the
-// count of the number of 'likes'
-// associated with that number displayed.
-heartButton.addEventListener('click', likeNumber);
-
-// 4. As a user, I can pause the counter, which should:
-//     *
-//     pause the counter *
-//     disable all buttons except the pause button *
-//     switch the label on the button from "pause"
-// to "resume"
-pauseButton.addEventListener('click', pauseTimer);
-
-// 5. As a user, I should be able to click the "restart"
-// button to restart the
-// counter and re - enable the buttons.
-restartButton.addEventListener('click', restart)
-
-// 6. As a user, I can leave comments on my gameplay, such as: "Wow, what a fun
-// game this is.
-// "
-submitButton.addEventListener('click', addComment)
+function handleSubmit(event) {
+    event.preventDefault()
+    const comment = event.target.querySelector("input").value
+    const li = document.createElement("li")
+    li.innerText = comment
+    comments.append(li)
+    event.target.reset()
+}
